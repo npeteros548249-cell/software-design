c09354b9cf0622b3f8f6bcd6196dc99e4c4446e7
maezing <n.peteros.548249@umindanao.edu.ph>
2026-03-17 11:03:57 +0800

    week 3

--- lab3/Form1.Designer.cs
+++ lab3/Form1.Designer.cs
@@ -20,20 +20,65 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBoxBooks = new ListBox();
+            btnShowInfo = new Button();
+            btnDisplayBooks = new Button();
+            SuspendLayout();
+            // 
+            // listBoxBooks
+            // 
+            listBoxBooks.ForeColor = SystemColors.WindowText;
+            listBoxBooks.FormattingEnabled = true;
+            listBoxBooks.Location = new Point(12, 12);
+            listBoxBooks.Name = "listBoxBooks";
+            listBoxBooks.Size = new Size(437, 304);
+            listBoxBooks.TabIndex = 0;
+            // 
+            // btnShowInfo
+            // 
+            btnShowInfo.Location = new Point(465, 25);
+            btnShowInfo.Name = "btnShowInfo";
+            btnShowInfo.Size = new Size(142, 64);
+            btnShowInfo.TabIndex = 1;
+            btnShowInfo.Text = "Show Message Box";
+            btnShowInfo.UseVisualStyleBackColor = true;
+            btnShowInfo.Click += btnShowInfo_Click;
+            // 
+            // btnDisplayBooks
+            // 
+            btnDisplayBooks.Location = new Point(73, 333);
+            btnDisplayBooks.Name = "btnDisplayBooks";
+            btnDisplayBooks.Size = new Size(142, 64);
+            btnDisplayBooks.TabIndex = 2;
+            btnDisplayBooks.Text = "Show Array of Books";
+            btnDisplayBooks.UseVisualStyleBackColor = true;
+            btnDisplayBooks.Click += btnDisplayBooks_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(749, 572);
+            Controls.Add(btnDisplayBooks);
+            Controls.Add(btnShowInfo);
+            Controls.Add(listBoxBooks);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private ListBox listBoxBooks;
+        private Button btnShowInfo;
+        private Button btnDisplayBooks;
+    }
 }
--- lab3/Form1.cs
+++ lab3/Form1.cs
@@ -1,10 +1,99 @@
+using Microsoft.VisualBasic.Devices;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+
 namespace lab3
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Book
+        {
+            public string Title { get; set; }
+
+            public string Author { get; set; }
+            public virtual string GetInfo()
+            {
+                return $"{Title} by {Author}";
+            }
+
+        }
+
+        public class Magazine : Book
+        {
+
+            public int IssueNumber { get; set; }
+
+            public override string GetInfo()
+            {
+                return $"{Title} - Issue {IssueNumber}";
+            }
+        }
+
+        public class Ebooks : Book
+        {
+            public double Size { get; set; }
+
+            public override string GetInfo()
+            {
+                return $"{Title} Size = {Size} mb";
+            }
+        }
+
+        private void btnShowInfo_Click(object sender, EventArgs e)
+        {
+            Book mybook = new Magazine()
+            {
+                Title = "Tech Weekly",
+                Author = "John Doe",
+                IssueNumber = 45
+            };
+            MessageBox.Show(mybook.GetInfo());
+
+        }
+
+        private void btnDisplayBooks_Click(object sender, EventArgs e)
+        {
+            Book[] Inventory = new Book[] 
+            { 
+                new Book {Title = "The Great Gatsby", Author = "F Scott Fritzgeral"},
+                new Magazine{Title = "National Geographic", IssueNumber = 2023},
+                new Ebooks {Title = "Digital Forest", Author = "Dan Brown", Size = 1.5},
+                new TextBook {Title = "Digital Forest", Subject = "Computer"},
+                new AudioBook {Title = "The Song of Achilles", Narrator = "Kai Shido", Duration = 20}
+            
+            };
+
+            foreach (Book book in Inventory)
+            {
+                listBoxBooks.Items.Add(book.GetInfo());
+                
+            }
+        }
+
+        public class TextBook : Book
+        {
+            public string Subject { get; set; }
+
+            public override string GetInfo()
+            {
+                return $"{Title} for {Subject}";
+            }    
+        }
+
+        public class AudioBook : Book 
+        {
+            public int Duration { get; set; }
+            public string Narrator {  get; set; }
+
+            public override string GetInfo()
+            {
+                return $"{Title} Narrated by: {Narrator} - {Duration} minutes";
+            }
+        }
+    }
 }
